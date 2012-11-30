6d212c26bc4221c9f63fa31d347aacd85e410403
Amman <ammanwaheedmughal@gmail.com>
2012-11-30 12:42:35 +0500

    Logo changed

--- application/forms/Signup.php
+++ application/forms/Signup.php
@@ -10,20 +10,29 @@ class Application_Form_Signup extends Zend_Form
 
         $fullname = new Zend_Form_Element_Text('fullname');
         $fullname->setRequired(TRUE);
+		$fullname->class='forminputsinput';
+		
 
         $email = new Zend_Form_Element_Text('email');
+		$email->class='forminputsinput';
         $email->addValidator('EmailAddress')
               ->setRequired(TRUE);
+			  
 
         $password = new Zend_Form_Element_Password('password');
         $password->setRequired(TRUE);
+		$password->class='forminputsinput';
 
         $zipcode = new Zend_Form_Element_Text('zipcode');
         $zipcode->setRequired(TRUE);
+		$zipcode->class='forminputsinput';
 
          $newsletter = new Zend_Form_Element_Checkbox('newsletter');
+		 $newsletter->class = "chkbox";
 
-         $submit = new Zend_Form_Element_Submit('submit');
+         $submit = new Zend_Form_Element_Submit('i');
+		 $submit->class='chkboxbutton';
+		 
 
          $this->addElements(array(
 
